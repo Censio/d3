d67e8013d62a495183d419d9e591e70ea07c7ae0
Jonas Buyl <jonas.buyl@gmail.com>
2017-09-28 17:45:08 +0200

    change time scale format

--- d3.js
+++ d3.js
@@ -9485,9 +9485,9 @@
     return d.getMilliseconds();
   } ], [ ":%S", function(d) {
     return d.getSeconds();
-  } ], [ "%I:%M", function(d) {
+  } ], [ "%H:%M", function(d) {
     return d.getMinutes();
-  } ], [ "%I %p", function(d) {
+  } ], [ "%H:%M", function(d) {
     return d.getHours();
   } ], [ "%a %d", function(d) {
     return d.getDay() && d.getDate() != 1;
